81221960d2f785cef3e26ade350e852cc6a0a2b0
Serk92 <a.diiacovo@studenti.luiss.it>
2026-03-26 17:51:44 +0100

    Implement drive parameter in SCREAMERAudioProcessor
    Added parameter handling for 'drive' in the audio processor.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,15 +1,6 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin processor.
-
-  ==============================================================================
-*/
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-//==============================================================================
 SCREAMERAudioProcessor::SCREAMERAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
      : AudioProcessor (BusesProperties()
@@ -19,9 +10,9 @@ SCREAMERAudioProcessor::SCREAMERAudioProcessor()
                       #endif
                        .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
                      #endif
-                       ),
-        apvts (*this, nullptr, "PARAMETERS", createParameters())
+                       )
 #endif
+    , apvts (*this, nullptr, "PARAMETERS", createParameters())
 {
 }
 
@@ -29,7 +20,19 @@ SCREAMERAudioProcessor::~SCREAMERAudioProcessor()
 {
 }
 
-//==============================================================================
+juce::AudioProcessorValueTreeState::ParameterLayout SCREAMERAudioProcessor::createParameters()
+{
+    std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
+
+    params.push_back (std::make_unique<juce::AudioParameterFloat>(
+        "drive",
+        "Drive",
+        juce::NormalisableRange<float> (1.0f, 20.0f, 0.1f),
+        1.0f));
+
+    return { params.begin(), params.end() };
+}
+
 const juce::String SCREAMERAudioProcessor::getName() const
 {
     return JucePlugin_Name;
@@ -69,8 +72,7 @@ double SCREAMERAudioProcessor::getTailLengthSeconds() const
 
 int SCREAMERAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    return 1;
 }
 
 int SCREAMERAudioProcessor::getCurrentProgram()
@@ -91,78 +93,65 @@ void SCREAMERAudioProcessor::changeProgramName (int index, const juce::String& n
 {
 }
 
-//==============================================================================
 void SCREAMERAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
 }
 
 void SCREAMERAudioProcessor::releaseResources()
 {
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
 bool SCREAMERAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
 {
-  #if JucePlugin_IsMidiEffect
+   #if JucePlugin_IsMidiEffect
     juce::ignoreUnused (layouts);
     return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
+   #else
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
+    #if ! JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
-   #endif
+    #endif
 
     return true;
-  #endif
+   #endif
 }
 #endif
 
-void SCREAMERAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void SCREAMERAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
+                                           juce::MidiBuffer& midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
+    juce::ignoreUnused (midiMessages);
+
+    auto totalNumInputChannels = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
+    auto* driveParam = apvts.getRawParameterValue ("drive");
+    const float drive = driveParam != nullptr ? driveParam->load() : 1.0f;
+
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
         auto* channelData = buffer.getWritePointer (channel);
 
-        // ..do something to the data...
+        for (int sample = 0; sample < buffer.getNumSamples(); ++sample)
+        {
+            const float x = channelData[sample] * drive;
+            channelData[sample] = std::tanh (x);
+        }
     }
 }
 
-//==============================================================================
 bool SCREAMERAudioProcessor::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+    return true;
 }
 
 juce::AudioProcessorEditor* SCREAMERAudioProcessor::createEditor()
@@ -170,22 +159,24 @@ juce::AudioProcessorEditor* SCREAMERAudioProcessor::createEditor()
     return new SCREAMERAudioProcessorEditor (*this);
 }
 
-//==============================================================================
 void SCREAMERAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    if (auto state = apvts.copyState(); state.isValid())
+    {
+        std::unique_ptr<juce::XmlElement> xml (state.createXml());
+        copyXmlToBinary (*xml, destData);
+    }
 }
 
 void SCREAMERAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+
+    if (xmlState != nullptr)
+        if (xmlState->hasTagName (apvts.state.getType()))
+            apvts.replaceState (juce::ValueTree::fromXml (*xmlState));
 }
 
-//==============================================================================
-// This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new SCREAMERAudioProcessor();
